517995e02f122072975bcbabcb15c8b387c9a96f
sadhj-art <1953269257@qq.com>
2025-01-18 21:17:24 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,30 +1,61 @@
-from openai import OpenAI
- 
-client = OpenAI(
-    api_key = "sk-jCdPRBTqzxCkWQz2LWeQ1iFAKuscawgUULUcxcR9xloT6tV0",
-    base_url = "https://api.moonshot.cn/v1",
-)
- 
-history = [
-    {"role": "system", "content": "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，不可翻译成其他语言。"}
-]
- 
-def chat(query, history):
-    history.append({
-        "role": "user", 
-        "content": query
-    })
-    completion = client.chat.completions.create(
-        model="moonshot-v1-8k",
-        messages=history,
-        temperature=0.3,
-    )
-    result = completion.choices[0].message.content
-    history.append({
-        "role": "assistant",
-        "content": result
-    })
-    return result
- 
-print(chat("地球的自转周期是多少？", history))
-print(chat("月球呢？", history))
+import os
+
+import requests
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# API 申请 https://platform.moonshot.cn
+authorization = os.getenv("moon_shot_authorization")
+headers = {
+    "Authorization": f"Bearer {authorization}"
+}
+api_url = "https://api.moonshot.cn/v1/chat/completions"
+api_key = "sk-jCdPRBTqzxCkWQz2LWeQ1iFAKuscawgUULUcxcR9xloT6tV0"
+
+app = FastAPI()
+system_message = "现在请你扮演一温柔善良，体贴，热心的虚拟女友聊天伴侣，需要陪我不断的聊天，提供情绪价值，回答的话语需要简短干练一点。"
+
+
+class ChatParams(BaseModel):
+    message: str
+
+
+# 全局变量 自己的女朋友只给自己用
+messages = [{"role": "system", "content": system_message}]
+
+
+@app.post("/chat_with_girlfriend")
+async def outpaint(params: ChatParams):
+    global messages
+    messages.append({"role": "user", "content": params.message})
+
+    # 保持消息条数不超过5条，但始终保留system消息
+    if len(messages) > 6:
+        # 仅截取用户和助手的最后4条消息，再与系统消息合并
+        messages = [messages[0]] + messages[-5:]
+
+    data = {
+        "model": "moonshot-v1-32k",
+        "messages": messages
+    }
+    try:
+        # 发送请求
+        print(data)
+        resp = requests.post(api_url, headers=headers, json=data)
+        resp.raise_for_status()  # 确保请求成功
+        girlfriend_reply = resp.json()["choices"][0]["message"]["content"]
+
+        # 将AI回复加入会话列表
+        messages.append({"role": "assistant", "content": girlfriend_reply})
+
+        # 返回AI的回复
+        return {"message": girlfriend_reply}
+    except requests.RequestException as e:
+        # 请求失败的处理
+        return {"error": "请求失败", "details": str(e)}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8888)
